09eb7c2bcb87690f00513594cf14da18c8296bb5
IEEE Kastoria <ieeesb@kastoria.teiwm.gr>
2018-10-09 10:50:24 +0300

    Initial commit

--- renderer.js
+++ renderer.js
@@ -1,12 +1,34 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-const serialport = require('serialport')
+const SerialPort = require('serialport')
 const createTable = require('data-table')
+const Readline = require('@serialport/parser-readline')
+let initialized = false;
+let port;
 
-serialport.list((err, ports) => {
-  console.log('ports', ports);
+let liveData = [];
+
+document.querySelector('#connect').addEventListener('click', () => {
+  port = new SerialPort(document.querySelector('#port').value,
+    {
+      autoOpen: false,
+      baudRate: 9600,
+    }
+  );
+
+  port.open(function (err) {
+    if (err) {
+      return console.log('Error opening port: ', err.message);
+    }
+
+    port.write('main screen turn on');
+  });
+
+  const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+  parser.on('data', renderData);
+
+  document.querySelector('#connection').classList.add('hidden')
+});
+
+SerialPort.list((err, ports) => {
   if (err) {
     document.getElementById('error').textContent = err.message
     return
@@ -23,6 +45,43 @@ serialport.list((err, ports) => {
   tableHTML = ''
   table.on('data', data => tableHTML += data)
   table.on('end', () => document.getElementById('ports').innerHTML = tableHTML)
-  ports.forEach(port => table.write(port))
+  ports.forEach(port => {
+    table.write(port)
+  })
   table.end();
-})
+});
+
+
+function renderData(data) {
+  if (!initialized) {
+    if (data.includes('SETUP END')) initialized = true;
+
+    return;
+  }
+  const headers = ['#', 'Wind direction', 'Wind speed', 'Humidity', 'Temprature', 'Rainfall', 'Pressure', 'Brightness']
+  const table = createTable(headers)
+  tableHTML = ''
+  table.on('data', data => tableHTML += data)
+  table.on('end', () => document.getElementById('ports').innerHTML = tableHTML)
+
+  const temp = data.split('|');
+  const obj = {};
+  temp.forEach(t => {
+    obj[t.split(':')[0].trim()] = t.split(':')[1];
+  });
+
+  liveData.push(obj);
+
+  if (liveData.length > 30) {
+    liveData.shift();
+  }
+  liveData.forEach((ld, index) => {
+    console.log(index);
+    ld['#'] = index + 1;
+    table.write(ld)
+  });
+
+
+
+  table.end();
+}
